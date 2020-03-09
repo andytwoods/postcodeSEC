ecf303a7c7d472bbf7caa4be0754761223b3cc44
andytwoods <andytwood@gmail.com>
2020-03-09 10:09:45 +0000

    more complicated than thought

--- conversion.py
+++ conversion.py
@@ -8,17 +8,23 @@ from tqdm import tqdm
 postcode_lookup = {}
 details_lookup = {}
 
-# unzip https://opendatacommunities.org/imd-files/imd2019lsoa.csv?uri=http%3A%2F%2Fopendatacommunities.org%2Fdata%2Fsocietal-wellbeing%2Fimd2019%2Findices and place in data folder
+# unzip https://www.arcgis.com/sharing/rest/content/items/c479d770cba14845a0e43db4e3eb5afa/data and place in data folder
+
+# https://assets.publishing.service.gov.uk/government/uploads/system/uploads/attachment_data/file/853811/IoD2019_FAQ_v4.pdf p12
 
 with open('data/PCD_OA_LSOA_MSOA_LAD_FEB19_UK_LU.csv', 'r') as f:
     data = csv.reader(f)
     headers = next(data)
 
+    # https://en.wikipedia.org/wiki/Lower_Layer_Super_Output_Area 1000-1500 people
     row_i = headers.index('lsoa11cd')
 
     for row in tqdm(data, total=2624585):
-        postcode = row[row_i]
-        postcode_lookup[postcode] = row
+        postcode_lookup_field = row[row_i]
+        #postcode = row[0].replace(' ', '')
+        if postcode_lookup_field in postcode_lookup:
+            raise Exception('duplicate postcode_lookup_field: ' + postcode_lookup_field)
+        postcode_lookup[postcode_lookup_field] = row
 
 with open('data/imd2019lsoa.csv', 'r') as f:
     data = csv.reader(f)
@@ -39,27 +45,27 @@ with open('data/imd2019lsoa.csv', 'r') as f:
 
         linking_header = row[row_i]
         postcode_info = postcode_lookup[linking_header]
-        postcode = postcode_info[postcode_i].replace(' ', '')
+        postcode_lookup_field = postcode_info[postcode_i].replace(' ', '')
 
         index_of_deprivation = row[index_of_deprivation_i]
         index_of_deprivation = index_of_deprivation[3:]  # first 3 characters are a. b. c. etc and so not useful
         measurement = row[measurement_i]
         value = row[value_i]
 
-        if postcode not in details_lookup:
-            details_lookup[postcode] = {'postcode_info': postcode_info, }
+        if postcode_lookup_field not in details_lookup:
+            details_lookup[postcode_lookup_field] = {'postcode_info': postcode_info, }
 
         if indices_to_include_use:
             if index_of_deprivation not in indices_to_include:
                 continue
 
-        if index_of_deprivation not in details_lookup[postcode]:
-            details_lookup[postcode][index_of_deprivation] = {}
+        if index_of_deprivation not in details_lookup[postcode_lookup_field]:
+            details_lookup[postcode_lookup_field][index_of_deprivation] = {}
 
-        if index_of_deprivation not in details_lookup[postcode][index_of_deprivation]:
-            details_lookup[postcode][index_of_deprivation][index_of_deprivation] = {}
+        if index_of_deprivation not in details_lookup[postcode_lookup_field][index_of_deprivation]:
+            details_lookup[postcode_lookup_field][index_of_deprivation][index_of_deprivation] = {}
 
-        details_lookup[postcode][index_of_deprivation][index_of_deprivation][measurement] = value
+        details_lookup[postcode_lookup_field][index_of_deprivation][index_of_deprivation][measurement] = value
 
 # let's group similar postcodes
 min_postcode_characters_to_group_by = 2
@@ -70,11 +76,11 @@ for postcode_characters_to_group_by in range(min_postcode_characters_to_group_by
 
     grouped = {}
 
-    for postcode, data in details_lookup.items():
-        abbrev_postcode = postcode[:postcode_characters_to_group_by]
+    for postcode_lookup_field, data in details_lookup.items():
+        abbrev_postcode = postcode_lookup_field[:postcode_characters_to_group_by]
         if abbrev_postcode not in grouped:
             grouped[abbrev_postcode] = []
-        grouped[abbrev_postcode].append({postcode: data})
+        grouped[abbrev_postcode].append({postcode_lookup_field: data})
 
     Path("/grouped").mkdir(parents=True, exist_ok=True)
 
